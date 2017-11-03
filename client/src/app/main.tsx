10aa0553c875fa2d92c88d668cb3364e18eeff96
Jokke Liikkanen <quutticode@gmail.com>
2017-11-03 22:32:03 +0200

    Init redux

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as redux from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+
+import combinedReducers from "./reducers/_combined";
+
+const store = redux.createStore(combinedReducers, redux.applyMiddleware(thunkMiddleware));
 
 ReactDOM.render((
-    <div>Test</div>
+    <Provider store={store}>
+        <div>Test</div>
+    </Provider>
 ), document.getElementById("app"))
