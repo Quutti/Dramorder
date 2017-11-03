403cae22671e1e0f6e68085d93a13c6fcaa0ccb8
Jokke Liikkanen <quutticode@gmail.com>
2017-11-03 22:03:08 +0200

    First implementation of interfaces for redux

---
+++ client/src/app/types.ts
@@ -0,0 +1,46 @@
+export interface RootState {
+    activeOrder: ActiveOrderState;
+    orderList: OrderListState;
+}
+
+export const ORDER_LIST_FETCHING = "ORDER_LIST_FETCHING";
+export const ORDER_LIST_RECEIVED = "ORDER_LIST_RECEIVED";
+export const ORDER_LIST_FAILED = "ORDER_LIST_FAILED";
+
+export interface OrderListState {
+    isFetching: boolean;
+    orders: PrunedOrder[];
+}
+
+export interface PrunedOrder {
+    id: number;
+    name: string;
+    createdAt: string;
+}
+
+export interface ActiveOrderState {
+    isFetching: boolean;
+    order: Order;
+}
+
+export interface Order extends PrunedOrder {
+    currencyMultiplier: number;
+    status: string;
+    lists: OrderList[];
+}
+
+export interface OrderList {
+    id: number;
+    name: string;
+    added: string;
+    items: OrderItem[]
+}
+
+export interface OrderItem {
+    id: number;
+    name: string;
+    href: string;
+    price: number;
+    quantity: number;
+    added: string;
+}
